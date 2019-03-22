f938510f82d474e218c8f453ad8a29bea640810e
Wang Zhe <wx51yz@qq.com>
2019-03-22 17:32:44 +0800

    重构baseDialog页面，使用teaset的Overlay.PullView

--- view/InputDialog.js
+++ view/InputDialog.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  ScrollView,
   TouchableOpacity,
   TextInput,
   Keyboard
@@ -40,134 +41,144 @@ class InputDialog extends BaseDialog {
     return { justifyContent: 'flex-end', alignItems: 'center' };
   }
 
-  show(text) {
-    super.show(null, { text: text });
-    this.textInput.focus();
+  show() {
+    super.show();
   }
 
-  dismiss(callback) {
-    this.textInput.blur();
-    super.dismiss(callback);
+  dismiss() {
+    super.close();
   }
 
   renderContent() {
     return (
-      <View style={{ width: this.mScreenWidth, backgroundColor: '#f8f8f8' }}>
-        <View
-          style={{
-            width: this.mScreenWidth,
-            height: this.getSize(50),
-            flexDirection: 'row',
-            paddingLeft: this.getSize(10),
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderBottomColor: '#F4F4F4',
-            borderBottomWidth: 1,
-            borderTopLeftRadius: 5,
-            borderTopRightRadius: 5
-          }}
-        >
-          <TouchableOpacity
-            onPress={() =>
-              this.dismiss(() => {
-                this.props.onCancel && this.props.onCancel();
-              })
-            }
+      <View
+        style={{
+          width: this.mScreenWidth,
+          backgroundColor: '#f8f8f8',
+          borderTopLeftRadius: 5,
+          borderTopRightRadius: 5
+        }}
+      >
+        <ScrollView keyboardShouldPersistTaps="always">
+          <View
             style={{
-              position: 'absolute',
-              left: this.getSize(10),
-              height: this.getSize(40),
+              width: this.mScreenWidth,
+              height: this.getSize(50),
               flexDirection: 'row',
+              paddingLeft: this.getSize(10),
               justifyContent: 'center',
               alignItems: 'center',
-              marginLeft: this.getSize(5)
+              borderBottomColor: '#F4F4F4',
+              borderBottomWidth: 1,
+              borderTopLeftRadius: 5,
+              borderTopRightRadius: 5
             }}
           >
-            <Text
+            <TouchableOpacity
+              onPress={() => {
+                Keyboard.dismiss();
+                this.dismiss(() => {
+                  this.props.onCancel && this.props.onCancel();
+                });
+              }}
               style={{
-                fontSize: this.props.cancelSize,
-                color: this.props.cancelColor,
+                position: 'absolute',
+                left: this.getSize(10),
+                height: this.getSize(40),
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
                 marginLeft: this.getSize(5)
               }}
             >
-              {this.props.cancelText}
-            </Text>
-          </TouchableOpacity>
-          <Text
-            style={{
-              position: 'absolute',
-              fontSize: this.props.titleSize,
-              color: this.props.titleColor,
-              fontWeight: '600'
-            }}
-          >
-            {this.props.title}
-          </Text>
-        </View>
-        <TextInput
-          ref={ref => (this.textInput = ref)}
-          style={{
-            width: this.getSize(345),
-            marginLeft: this.getSize(15),
-            height:
-              this.props.numberOfLines > 1
-                ? this.getSize(100)
-                : this.getSize(20),
-            color: '#333333',
-            fontSize: this.getSize(14),
-            borderWidth: 1,
-            borderColor: '#E8EEF0',
-            backgroundColor: '#ffffff',
-            borderRadius: this.getSize(4),
-            paddingLeft: this.getSize(15),
-            paddingRight: this.getSize(15),
-            paddingTop: this.getSize(10)
-          }}
-          numberOfLines={this.props.numberOfLines}
-          multiline={this.props.numberOfLines > 1}
-          value={this.state.text}
-          underlineColorAndroid={'transparent'}
-          placeholder={this.props.placeholder}
-          placeholderTextColor="#999999"
-          onChangeText={text => {
-            this.inputText = text;
-          }}
-        />
-        <View
-          style={{
-            width: this.mScreenWidth,
-            height: this.getSize(48),
-            paddingRight: this.getSize(15),
-            justifyContent: 'center',
-            alignItems: 'flex-end'
-          }}
-        >
-          <TouchableOpacity
-            onPress={() =>
-              this.dismiss(() => {
-                this.props.onSubmit && this.props.onSubmit(this.inputText);
-              })
-            }
-            style={{
-              width: this.getSize(60),
-              height: this.getSize(28),
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: this.props.btnBgColor,
-              borderRadius: this.getSize(4)
-            }}
-          >
+              <Text
+                style={{
+                  fontSize: this.props.cancelSize,
+                  color: this.props.cancelColor,
+                  marginLeft: this.getSize(5)
+                }}
+              >
+                {this.props.cancelText}
+              </Text>
+            </TouchableOpacity>
             <Text
               style={{
-                fontSize: this.props.btnTextSize,
-                color: this.props.btnTextColor
+                position: 'absolute',
+                fontSize: this.props.titleSize,
+                color: this.props.titleColor,
+                fontWeight: '600'
               }}
             >
-              {this.props.btnText}
+              {this.props.title}
             </Text>
-          </TouchableOpacity>
-        </View>
-        {this.props.modal ? null : <KeyboardSpacer />}
+          </View>
+          <TextInput
+            autoFocus={true}
+            ref={ref => (this.textInput = ref)}
+            style={{
+              width: this.getSize(345),
+              marginLeft: this.getSize(15),
+              height:
+                this.props.numberOfLines > 1
+                  ? this.getSize(100)
+                  : this.getSize(40),
+              color: '#333333',
+              fontSize: this.getSize(14),
+              borderWidth: 1,
+              borderColor: '#E8EEF0',
+              backgroundColor: '#ffffff',
+              borderRadius: this.getSize(4),
+              paddingLeft: this.getSize(15),
+              paddingRight: this.getSize(15),
+              paddingTop: this.getSize(10)
+            }}
+            numberOfLines={this.props.numberOfLines}
+            multiline={this.props.numberOfLines > 1}
+            value={this.state.text}
+            underlineColorAndroid={'transparent'}
+            placeholder={this.props.placeholder}
+            placeholderTextColor="#999999"
+            onChangeText={text => {
+              this.inputText = text;
+            }}
+          />
+          <View
+            style={{
+              width: this.mScreenWidth,
+              height: this.getSize(48),
+              paddingRight: this.getSize(15),
+              justifyContent: 'center',
+              alignItems: 'flex-end'
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => {
+                Keyboard.dismiss();
+                this.dismiss(() => {
+                  this.props.onSubmit && this.props.onSubmit(this.inputText);
+                });
+              }}
+              style={{
+                width: this.getSize(60),
+                height: this.getSize(28),
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: this.props.btnBgColor,
+                borderRadius: this.getSize(4)
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: this.props.btnTextSize,
+                  color: this.props.btnTextColor
+                }}
+              >
+                {this.props.btnText}
+              </Text>
+            </TouchableOpacity>
+          </View>
+          {this.props.modal ? null : <KeyboardSpacer />}
+        </ScrollView>
       </View>
     );
   }
